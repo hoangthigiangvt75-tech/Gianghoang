f62f68c90f6332665e608d6b456be01aacaf72cf
hoangthigiangvt75-tech <hoangthigiangvt75@gmail.com>
2026-03-12 16:12:43 +0700

    app.py

--- app.py
+++ app.py
@@ -191,7 +191,7 @@ st.markdown("""
 st.markdown("""
 <div class="main-header">
     <h1>📘 TRỢ LÝ SOẠN GIÁO ÁN TỰ ĐỘNG (NLS)</h1>
-    <p>Tác giả: Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</p>
+    <p>Tác giả: Hoàng Thị Giang - Trường  Tiểu học Vĩnh thành - ĐT: 0913784075 343</p>
 </div>
 """, unsafe_allow_html=True)
 
@@ -347,4 +347,4 @@ if st.button("🚀 SOẠN GIÁO ÁN NGAY"):
 
 # --- CHÂN TRANG ---
 st.markdown("---")
-st.markdown("<div style='text-align: center; color: #666;'>© 2025 - Lù Seo Sần - Trường PTDTBT Tiểu học Bản Ngò - ĐT: 097 1986 343</div>", unsafe_allow_html=True)
+st.markdown("<div style='text-align: center; color: #666;'>© 2025 - hoàng Thị Giang - Trường  Tiểu học Vĩnh Thành - ĐT: 0913784075 343</div>", unsafe_allow_html=True)
